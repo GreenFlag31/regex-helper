b6ec6b7c9444f62a404434e04eb8baa99e8fa3b1
unknown <unam31@gmail.com>
2024-08-07 12:54:19 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
-import express from 'express';
-import { log } from 'console';
-import {
-  amount,
-  anyDigit,
-  anyDigits,
-  anyWords,
-  emailAdress,
-  EUFullDate,
-  EUFullDateWithDashes,
-  phoneNumber,
-  URLSchema,
-} from './const';
-import { RegexHelper } from './regex-helper';
-
 export type {
   Options,
   RegexInit,
@@ -41,31 +26,9 @@ export {
   nonWordBoundary,
   optionalSpacings,
   phoneNumber,
+  spacing,
   spacings,
   wordBoundary,
   year,
 } from './const';
 export { RegexHelper } from './regex-helper';
-
-const app = express();
-
-const text = `The article: 471 has been paid on 12/12/2022. This message has been sent to email@email.com and test@test.com. Payment was made via credit card number 1234 5678 9012 3456. The total amount was $123.45. You can view your order history at https://example.com/orders. Service type: ordinary, number: 12345, phone: +49123456578.`;
-
-app.get('/pdf-to-png', (req, res) => {
-  const regex = new RegexHelper({
-    regex: `${EUFullDate}`,
-    name: 'EUFullDate',
-  })
-    .query({
-      regex: `Service type: ${anyWords}, number: ${anyDigits}, phone: ${phoneNumber}.`,
-      name: 'articleOrService',
-    })
-    .findIn(text)
-    .get('general');
-
-  res.send(regex);
-});
-
-app.listen(3006, () => {
-  console.log('listening on *:3006');
-});
